6fff5d716719fb4624f560528509a977467984c6
583660496 <583660496@qq.com>
2015-05-20 03:05:56 -0700

    201505201805

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,13 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.page(params[:page]).per(5)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @events.to_xml }
+      format.json { render :json => @events.to_json }
+      format.atom { @feed_title = "My event list" } # index.atom.builder
+    end
   end
   
   def new
@@ -12,9 +19,9 @@ class EventsController < ApplicationController
   def create 
     @event = Event.new(event_params)
     if @event.save
-      redirect_to :action => :index
+      redirect_to events_url
     else
-      render :action => :new
+      render new_event_url
     end
     flash[:notice] = "event was successfully created"
   end
@@ -22,6 +29,11 @@ class EventsController < ApplicationController
   def show
 #    @event = Event.find(params[:id]) 
     @page_title = @event.name
+    respond_to do |format|
+      format.html { @page_title = @event.name } # show.html.erb
+      format.xml # show.xml.builder
+      format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+    end
   end
 
   def edit
@@ -31,9 +43,9 @@ class EventsController < ApplicationController
   def update
 #    @event = Event.find(params[:id])
     if @event.update(event_params)
-      redirect_to :action => :show, :id => @event
+      redirect_to event_url(@event)
     else
-      render :action => :edit
+      render edit_event_url(@event)
     end
     flash[:notice] = "event was successfully updated"
   end
