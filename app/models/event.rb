85d753552b84325522f3cbfb885bf9b311eb80c8
583660496 <583660496@qq.com>
2015-05-21 02:55:32 -0700

    201505211755

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,9 @@ class Event < ActiveRecord::Base
   validates_presence_of :name
 #  has_many :attendees
 #  belongs_to :category
-#  has_one :location
-  has_many :event_groupships
-  has_many :groups, :through => :event_groupships
+  has_one :location
+#  has_many :event_groupships
+#  has_many :groups, :through => :event_groupships
+  delegate :name, :to => :category, :prefix => true, :allow_nil => true
+#  delegate :name, :to => :location, :prefix => true, :allow_nil => true
 end
